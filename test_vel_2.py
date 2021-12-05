cd11954e98825b68ff6007788e02f4c9f8c13fae
linavait <93937022+linavait@users.noreply.github.com>
2021-12-05 12:47:07 +0200

    daromi darbai pagal slide medž.

--- test_vel_2.py
+++ test_vel_2.py
@@ -4,14 +4,14 @@ from testvel import count_digits, count_zeros
 
 
 class TestDigits(unittest.TestCase):
-    def test_test_one_digit(self):
+    def test_one_digit(self):
         self.assertEqual(count_digits(5), 1, msg = 'neteisingai')
 
     def test_two_digits(self):
         self.assertEqual(count_digits(55), 2)
 
     def test_many_digits(self):
-        self.assertEqual(count_digits(111111111, 2)
+        self.assertEqual(count_digits(111111111, 2))
 
     def test_false_input(self):
           with self.assertRaises(ValueError):
